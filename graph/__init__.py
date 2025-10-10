e96ab6d35745a07d9670f44a89d145a6e7116dc9
kommanamanchi bharadwaj <kommanamanchibharadwaj@gmail.com>
2025-10-10 17:53:19 +0530

    adding details

---
+++ graph/__init__.py
@@ -0,0 +1,11 @@
+import sqlite3
+from langgraph.checkpoint.sqlite import SqliteSaver
+from graph.edges import graph_builder
+
+# Define the connection to the existing Sqlite database
+db_path = "example.db"
+conn = sqlite3.connect(db_path, check_same_thread=False)
+
+memory = SqliteSaver(conn)
+workflow = graph_builder()
+graph = workflow.compile(checkpointer=memory)
